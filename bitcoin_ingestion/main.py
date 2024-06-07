aef764567333f7379a56f45cff9b293e48f264d9
keymod <113419451+keymod@users.noreply.github.com>
2024-06-07 16:31:06 -0400

    Update main.py

--- bitcoin_ingestion/main.py
+++ bitcoin_ingestion/main.py
@@ -10,7 +10,7 @@ def connect():
 
         # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
-        
+        conn = psycopg2.connect(**params)
         
         cur = conn.cursor()
         
